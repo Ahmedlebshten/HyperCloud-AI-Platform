e8ae67e4f302613e656c491cf8d23338e1c3c570
Ahmedlebshten <ahmedlebshtenlebshten@gmail.com>
2026-04-27 14:52:03 +0300

    edit src code

--- app/app.js
+++ app/app.js
@@ -32,7 +32,7 @@ app.get('/', async (req, res) => {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ahmed's DevSecOps Portal V16</title>
+        <title>GuestBook Webapp</title>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
       <body class="bg-gray-900 text-white min-h-screen font-sans">
